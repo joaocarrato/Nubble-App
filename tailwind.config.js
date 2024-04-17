e412b24379ad9bd09793b2cc5ce583ef083c1e49
joaocarrato <joaocarrato18@gmail.com>
2024-04-17 17:18:02 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        graysBlack: '#000000',
+        graysOne: '#636363',
+        graysTwo: '#8E8E8E',
+        graysThree: '#B3B3B3',
+        graysFour: '#E1E1E1',
+        graysFive: '#F5F5F5',
+        graysWhite: '#FFFFFF',
+        primaryDefault: '#074C4E',
+        primaryLight: '#EAF6F6',
+        secondaryDefault: '#F86F2D',
+        secondaryLight: '#FAE6DD',
+        successDefault: '#4ABC86',
+        successLight: '#D8FFEC',
+        errorDefault: '#D8FFEC',
+        errorLight: '#FBECEC',
+      },
+      fontFamily: {
+        black: 'Satoshi-Black',
+        bold: 'Satoshi-Bold',
+        light: 'Satoshi-Light',
+        medium: 'Satoshi-Medium',
+        regular: 'Satoshi-Regular',
+      },
+    },
+  },
+  plugins: [],
+};
